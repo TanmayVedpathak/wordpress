d2a0095fcb2a92c40aaa937ca9c25c4ed2406740
TanmayVedpathak <tanmayvedpathak19@gmail.com>
2023-05-28 17:38:05 +0530

    final

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("gelo from js");
+$(document).ready(function () {
+  $(".items").slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
